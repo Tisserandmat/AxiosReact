267cf0034539b057ce9c88803ece6ca360c6a4a1
Tisserandmat <tisserand-mathias_student2020@wilder.school>
2021-10-18 17:22:27 +0200

    réalisation appel API via react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import TextAndButtonComponent from "./Components/TextAndButton"
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ const [images, setImages ] = useState([]) ;         // synthaxe de la déconstruction de l'object useState
+ useEffect (()=> {                                   // appellation de la function use effect
+   const fetchData = async () => {                    // déclaration de la fonction asyncrome 
+     try{                                             // en cas de reponse positive
+       const result = await axios.get("https://picsum.photos/v2/list")  // recupération de la reponse axios
+       setImages(result.data);                            // recherche de l'attribut data dans l'obect axios
+       console.log(result.data)                           //j'affiche les data dans la console
+     } catch (err) {
+       console.log(err)                                 //message d'érreur
+     }
+   };
+   fetchData();                             // utilisation de la methode que l'on vien de construire
+ }, [])                                    // si le tableau est vide la fonction qui est passer a use effect ne se passe qu'une fois
 
+
+  return images.map(({ author , id}) => <p>{author}, {id}</p>);                      // retourne une image map pour itéré chaque data contenu dans le use state
+
+}
 export default App;
+
---
+++ src/Components/TextAndButton.js
@@ -0,0 +1,9 @@
+const TextAndButtonComponent = ({name}) => {
+    return (
+        <>
+        <p>Hello je suis le composant {name}!!!</p>
+        <button>Clique mooi !</button>
+        </>
+    );
+}
+export default TextAndButtonComponent;
